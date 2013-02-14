f3cacc0e912ce45dc0fdf42bed7342cb837ebfc4
Meyer Thibaut <contact@thibaut-meyer.fr>
2013-02-14 23:03:04 +0100

    Update DataManager with Bonus implementation

--- ServeurBrute/DataManager.cs
+++ ServeurBrute/DataManager.cs
@@ -33,6 +33,19 @@ namespace ServeurBrute
                     xml.WriteElementString("Agility", Convert.ToString(brute.Agility));
                     xml.WriteElementString("Speed", Convert.ToString(brute.Speed));
                     xml.WriteElementString("Image", Convert.ToString(brute.Image));
+                    xml.WriteStartElement("BonusList");
+                    foreach (Bonus bonus in brute.BonusList)
+                    {
+                        xml.WriteStartElement("Bonus");
+                        xml.WriteElementString("Name", bonus.Name);
+                        xml.WriteElementString("Life", Convert.ToString(bonus.Life));
+                        xml.WriteElementString("Strength", Convert.ToString(bonus.Strength));
+                        xml.WriteElementString("Agility", Convert.ToString(bonus.Agility));
+                        xml.WriteElementString("Speed", Convert.ToString(bonus.Speed));
+                        xml.WriteElementString("Image", Convert.ToString(bonus.Image));
+                        xml.WriteEndElement();
+                    }
+                    xml.WriteEndElement();
                     xml.WriteEndElement();
                 }
                 xml.WriteEndElement();
@@ -53,21 +66,38 @@ namespace ServeurBrute
                 XmlTextReader xml = new XmlTextReader("Users.xml");
                 while (xml.Read())
                 {
-                    //  Here we check the type of the node, in this case we are looking for element
-                    if (xml.NodeType == XmlNodeType.Element)
+                    if (xml.Name.Equals("Brute") && (xml.NodeType == XmlNodeType.Element))
                     {
-                        //  If the element is "profile"
-                        if (xml.Name == "Brute")
+                        Console.WriteLine("Brute"+x);
+                        xml.Read();
+                        String name = xml.ReadElementString("Name");
+                        short level = Convert.ToInt16(xml.ReadElementString("Level"));
+                        short life = Convert.ToInt16(xml.ReadElementString("Life"));
+                        short strength = Convert.ToInt16(xml.ReadElementString("Strength"));
+                        short agility = Convert.ToInt16(xml.ReadElementString("Agility"));
+                        short speed = Convert.ToInt16(xml.ReadElementString("Speed"));
+                        int image = Convert.ToInt32(xml.ReadElementString("Image"));
+                        Brute brute = new Brute(name, level, life, strength, agility, speed, image);
+                        xml.Read();
+                        if(xml.Name.Equals("BonusList") && (xml.NodeType == XmlNodeType.Element))
                         {
-                            xml.Read();
-                            String name = xml.ReadElementString("Name");
-                            short level = Convert.ToInt16(xml.ReadElementString("Level"));
-                            short life = Convert.ToInt16(xml.ReadElementString("Life"));
-                            short strength = Convert.ToInt16(xml.ReadElementString("Strength"));
-                            short agility = Convert.ToInt16(xml.ReadElementString("Agility"));
-                            short speed = Convert.ToInt16(xml.ReadElementString("Speed"));
-                            int image = Convert.ToInt32(xml.ReadElementString("Image"));
-                            brutes.Add(name,new Brute(name, level, life, strength, agility, speed, image));
+                            XmlReader inner = xml.ReadSubtree();
+                            while (inner.Read())
+                            {
+                                if (inner.Name.Equals("Bonus") && (xml.NodeType == XmlNodeType.Element))
+                                {
+                                    Console.WriteLine("Bonus"+u);
+                                    xml.Read();
+                                    name = xml.ReadElementString("Name");
+                                    life = Convert.ToInt16(xml.ReadElementString("Life"));
+                                    strength = Convert.ToInt16(xml.ReadElementString("Strength"));
+                                    agility = Convert.ToInt16(xml.ReadElementString("Agility"));
+                                    speed = Convert.ToInt16(xml.ReadElementString("Speed"));
+                                    image = Convert.ToInt32(xml.ReadElementString("Image"));
+                                    brute.BonusList.Add(new Bonus(name, life, strength, agility, speed, image));
+                                }
+                            }
+                            inner.Close();
                         }
                     }
 
